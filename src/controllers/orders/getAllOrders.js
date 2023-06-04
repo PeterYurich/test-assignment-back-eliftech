76cf5f8c33c5172e65487d51c0c087da3ddfc87d
Peter <peteryurich@gmail.com>
2023-06-04 20:51:22 +0300

    getAllOrders fix 500

--- src/controllers/orders/getAllOrders.js
+++ src/controllers/orders/getAllOrders.js
@@ -1,19 +1,23 @@
+const { HttpError } = require("../../helpers")
 const { Order } = require("../../models/orderModel")
 const { User } = require("../../models/userModel")
 
 const getAllOrders = async (req, res, next) => {
-    const { email } = req.query
-      const { page = 1, limit = 10, favorite } = req.query
-      const user = await User.findOne({ email })
-      const CurrentUserId = user._id
-      const skip = (page - 1) * limit
+  const { email } = req.query
+  const { page = 1, limit = 10, favorite } = req.query
+  const user = await User.findOne({ email })
 
-      const query = { owner: CurrentUserId };
+  if (!user) { throw HttpError(404, "Not found") }
 
-      const result = await Order.find(query, "-createdAt -updatedAt", { skip, limit })
-        .populate("owner", "order")
+  const CurrentUserId = user._id
+  const skip = (page - 1) * limit
 
-    res.json(result)
+  const query = { owner: CurrentUserId };
+
+  const result = await Order.find(query, "-createdAt -updatedAt", { skip, limit })
+    .populate("owner", "order")
+
+  res.json(result)
 }
 
 module.exports = getAllOrders
